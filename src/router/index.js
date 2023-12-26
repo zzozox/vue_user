cd1987e4e139825e10d3c62d88c941c9894b0a53
zzozox <663482818@qq.com>
2023-12-27 03:15:03 +0800

    增加路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { unauthorized } from "@/net";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,44 +27,17 @@ const router = createRouter({
             path: '/userCenter',
             name: 'userCenter',
             component: () => import('@/views/userCenter.vue'),
-        },
-        // {
-        //     path: '/editUser',
-        //     name: 'editUser',
-        //     component: () => import('@/component/UserCenter/editUser.vue'),
-        // },{
-        //     path: '/message',
-        //     name: 'massage',
-        //     component: () => import('@/component/UserCenter/message.vue'),
-        // },{
-        //     path: '/addVideo',
-        //     name: 'addVideo',
-        //     component: () => import('@/component/UserCenter/addVideo.vue'),
-        // },{
-        //     path: '/followings',
-        //     name: 'followings',
-        //     component: () => import('@/component/UserCenter/followings.vue'),
-        // },{
-        //     path: '/followers',
-        //     name: 'followers',
-        //     component: () => import('@/component/UserCenter/followers.vue'),
-        // },{
-        //     path: '/myVideos',
-        //     name: 'myVideos',
-        //     component: () => import('@/component/UserCenter/myVideos.vue'),
-        // }
+        }
     ]
 })
 
-// router.beforeEach((to, from, next) => {
-//     const isUnauthorized = unauthorized()
-//     if(to.name.startsWith('index') && !isUnauthorized) {
-//         next('/index')
-//     } else if(to.fullPath.startsWith('/index') && isUnauthorized) {
-//         next('/')
-//     } else {
-//         next()
-//     }
-// })
+router.beforeEach((to, from, next) => {
+    const userId=sessionStorage.getItem("userId")
+    if(to.name.startsWith('videoPlay') && userId==null) {
+        next('/login')
+    } else {
+        next()
+    }
+})
 
 export default router
